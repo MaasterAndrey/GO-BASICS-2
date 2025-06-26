075488e0c87dbfb369146d2ce055864cfc63f44c
Andrei <zaharof.andrey@yandex.ru>
2025-06-26 11:56:54 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+// В цикле спрашиваем ввод транзакций: -10, 10, 40.5
+// Добавлять каждую в массив транзакций
+// Вывести массив
+// !Вывести сумму баланса в консоль
+
+func main() {
+	tr := make([]string, 0, 2)
+	fmt.Println(len(tr), cap(tr))
+	tr = append(tr, "1")
+	fmt.Println(len(tr), cap(tr))
+	tr = append(tr, "2")
+	fmt.Println(len(tr), cap(tr))
+	tr = append(tr, "3")
+	fmt.Println(tr)
+
+	transactions := []float64{}
+	for {
+		transaction := scanTransaction()
+		if transaction == 0 {
+			break
+		}
+		transactions = append(transactions, transaction)
+	}
+	balance := calculateBalance(transactions)
+	fmt.Printf("Ваш баланс: %.2f", balance)
+}
+
+func scanTransaction() float64 {
+	var transaction float64
+	fmt.Print("Введите транзакцию (n для выхода): ")
+	fmt.Scan(&transaction)
+	return transaction
+}
+
+func calculateBalance(transactions []float64) float64 {
+	balance := 0.0
+	for _, value := range transactions {
+		balance += value
+	}
+	return balance
+}
